719e841a1e40345bc4a8d95fd685c08c737b81c3
Shibo Xing <shx26@pitt.edu>
2022-01-11 23:32:02 -0500

    fix: variable collation

--- utils/OID_Dataset.py
+++ utils/OID_Dataset.py
@@ -5,15 +5,14 @@ import pandas as pd
 from torch.utils.data import Dataset as DS
 from PIL import Image
 
-
-class OID_Dataset(DS):
+class OID_Dataset(Dataset):
     
     def __init__(self, root, label_file, classes, dtype='TRAIN'):
         self.__root__ = root
         self.fnames = os.listdir(f'{root}/IMAGES/{dtype}/DATA/')
         self.labels = pd.read_csv(f'{root}/ANNOT/{label_file}')
         self.labels = self.labels[self.labels.LabelName.isin(classes)]
-        self.dtype = dtype
+        self.data_type = dtype
         
         self.trans_func = transforms.Compose([
             transforms.Resize([224,224]),
@@ -21,12 +20,13 @@ class OID_Dataset(DS):
             transforms.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5))
         ])
         
-        self.classes = dict([(b, a) for a, b in enumerate(classes)])
+        # assign an integer to each class name
+        self.classes = dict([(b, a) for a, b in enumerate(classes)]) 
         self.labels.LabelName = self.labels.LabelName.replace(self.classes)
         
     def __getitem__(self, ind):
         _id = self.fnames[ind].split('.')[0]
-        img_pth = f'{self.__root__}/IMAGES/{self.dtype}/DATA/{_id}.jpg'
+        img_pth = f'{self.__root__}/IMAGES/{self.data_type}/DATA/{_id}.jpg'
         img = Image.open(img_pth).convert('RGB')
         img = self.trans_func(img)
         label = self.labels[self.labels.ImageID == _id].iloc[:, 2:8]
