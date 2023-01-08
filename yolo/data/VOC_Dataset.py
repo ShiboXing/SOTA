817ab3004f0388653f8e4293922846eafd752103
Shibo Xing <shibox@amazon.com>
2023-01-08 22:14:59 +0000

    feat: transform coordinates in dataset, docstring

--- yolo/data/VOC_Dataset.py
+++ yolo/data/VOC_Dataset.py
@@ -1,18 +1,45 @@
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 from os.path import join
 from subprocess import run
-from torch.utils.data import Dataset as DS
+from PIL import Image
+from numpy import asarray
+from torch.utils.data import Dataset as DS, DataLoader as DL
+
 
 def get_bboxes(xml_pth):
+    """
+    get the coordinates from the xml in xml_pth
+
+    return Value:
+    dims -- xmin, ymin, xmax, ymax
+
+    """
     tree = ET.parse(xml_pth)
     objs = tree.findall(".//object")
     dims = []
     for o in objs:
         bbox = o.findall(".//bndbox/*")
         dims.append([int(n.text) for n in bbox])
-    
+
     return dims
+
+
+def get_pct_coords(bbox, img_dims):
+    """Keyword arguments:
+
+    bbox -- (xmin, ymin, xmax, ymax)
+    img_dims -- (xsize, ysize)
+    """
+    return (
+        float(bbox[0]) / img_dims[0],
+        float(bbox[1]) / img_dims[1],
+        float(bbox[2]) / img_dims[0],
+        float(bbox[3]) / img_dims[1],
+    )
+
 
 class VOC_Dataset(DS):
     def __init__(self, voc_root: str):
@@ -21,13 +48,25 @@ class VOC_Dataset(DS):
 
         res = run(f"ls {self.img_root}", shell=True, check=True, capture_output=True)
         self.imgs = res.stdout.decode("utf-8").split("\n")
-        
+
     def __getitem__(self, ind):
+        """
+        Return the resized image and its resized percentage coordinates
+        """
         img_file = self.imgs[ind]
         img_id = img_file.split(".")[0]
         label_file = f"{img_id}.xml"
-        
-        return (
-            join(self.img_root, img_file),
-            join(self.ant_root, label_file)
+        img_pth, ant_pth = join(self.img_root, img_file), join(
+            self.ant_root, label_file
         )
+
+        img = Image.open(img_pth)
+        print(img.size)
+        coords = get_bboxes(ant_pth)
+        pct_coords = []
+        for coord in coords:
+            pct_coords.append(get_pct_coords(coord, img.size))
+        res_img = img.resize((448, 448))
+        data = asarray(res_img)
+
+        return data, pct_coords
