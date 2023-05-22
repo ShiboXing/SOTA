f43fb1134bf615786b520fb738ebf79330eb727d
Shibo Xing <stanleyhsing26@gmail.com>
2023-05-21 23:48:14 -0400

    feat: add log returns

--- lstm/sales_data.py
+++ lstm/sales_data.py
@@ -1,11 +1,20 @@
 import torch
 import pandas as pd
 import os
+import numpy as np
 
+from ipdb import set_trace
 from torch.utils.data import Dataset as DS
 
 
 class Sales_Dataset(DS):
+    def get_log_ret(self, df: pd.DataFrame, y_col:str, sort_keys = []):
+        """Calculate in-place the log returns of y_col"""
+        df[y_col] += 0.1 # prevent division by zero
+        df.sort_values(by=sort_keys, inplace=True)
+        df[y_col] = np.log(df[y_col] / df[y_col].shift(1))
+
+        
     def z_series(self, df: pd.Series):
         """Normalize dataframe series while eliminating the effect of zeros"""
         df_tmp = df[df != 0.0]
@@ -13,7 +22,7 @@ class Sales_Dataset(DS):
             lower=df_tmp.mean() - 2 * df_tmp.std(),
             upper=df_tmp.mean() + 2 * df_tmp.std(),
         )
-        return (df - df.mean()) / df.std()
+        return (df - df.mean()) / df.std() - 1
 
     def parse_nominal(self, df: pd.Series):
         """Transform nominal data into numerical data"""
@@ -31,18 +40,22 @@ class Sales_Dataset(DS):
         ids = set()
         self.TR.apply(lambda row: ids.add((row["date"], row["store_nbr"])), axis=1)
         self.ids = sorted(list(ids))
-        self.TR.sort_values(["date", "store_nbr"], inplace=True)
 
         # preprocess data
         self.TR.family = self.z_series(self.parse_nominal(self.TR.family))
-        self.TR.sales = self.z_series(self.TR.sales)
+        self.get_log_ret(self.TR, "sales", ["store_nbr", "family", "date"])
         self.TR.onpromotion = self.z_series(self.TR.onpromotion)
+        self.promo_std, self.promo_mean = self.TR.onpromotion.std(), self.TR.onpromotion.mean()
 
-        self.TS.transactions = self.z_series(self.TS.transactions)
+        self.get_log_ret(self.TS, "transactions", ["store_nbr", "date"])
         self.S.city = self.z_series(self.parse_nominal(self.S.city))
         self.S.cluster = self.z_series(self.S.cluster)
         self.S.type = self.z_series(self.parse_nominal(self.S.type))
         self.S = self.S[["city", "cluster", "type"]]
+
+        self.TR.sort_values(["date", "store_nbr", "family"], inplace=True)
+        self.TS.sort_values(["date", "store_nbr"], inplace=True)
+        self.O.sort_values(["date"], inplace=True)
 
     def __len__(self):
         return len(self.ids)
@@ -52,7 +65,5 @@ class Sales_Dataset(DS):
         tr = self.TR[(self.TR.date == date) & (self.TR.store_nbr == store_nbr)]
         ts = self.TS[(self.TS.date == date) & (self.TR.store_nbr == store_nbr)]
         o = self.O[self.O.date == date]
-        
-        return (
-            tr, ts, o
-        )
+
+        return (tr, ts, o)
