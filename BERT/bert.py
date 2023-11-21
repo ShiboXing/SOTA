c9cb544e2d0a17f078370cf283e812647ddba0d3
Shibo Xing <shx26@pitt.edu>
2023-11-21 15:26:50 +0800

    feat: add pretrain dataset

--- BERT/bert.py
+++ BERT/bert.py
@@ -206,3 +206,122 @@ def _get_mlm_data_from_tokens(tokens, vocab):
     pred_positions = [v[0] for v in pred_positions_and_labels]
     mlm_pred_labels = [v[1] for v in pred_positions_and_labels]
     return vocab[mlm_input_tokens], pred_positions, vocab[mlm_pred_labels]
+
+
+def _pad_bert_inputs(examples, max_len, vocab):
+    max_num_mlm_preds = round(max_len * 0.15)
+    (
+        all_token_ids,
+        all_segments,
+        valid_lens,
+    ) = (
+        [],
+        [],
+        [],
+    )
+    all_pred_positions, all_mlm_weights, all_mlm_labels = [], [], []
+    nsp_labels = []
+    for token_ids, pred_positions, mlm_pred_label_ids, segments, is_next in examples:
+        all_token_ids.append(
+            torch.tensor(
+                token_ids + [vocab["<pad>"]] * (max_len - len(token_ids)),
+                dtype=torch.long,
+            )
+        )
+        all_segments.append(
+            torch.tensor(segments + [0] * (max_len - len(segments)), dtype=torch.long)
+        )
+        # `valid_lens` excludes count of '<pad>' tokens
+        valid_lens.append(torch.tensor(len(token_ids), dtype=torch.float32))
+        all_pred_positions.append(
+            torch.tensor(
+                pred_positions + [0] * (max_num_mlm_preds - len(pred_positions)),
+                dtype=torch.long,
+            )
+        )
+        # Predictions of padded tokens will be filtered out in the loss via
+        # multiplication of 0 weights
+        all_mlm_weights.append(
+            torch.tensor(
+                [1.0] * len(mlm_pred_label_ids)
+                + [0.0] * (max_num_mlm_preds - len(pred_positions)),
+                dtype=torch.float32,
+            )
+        )
+        all_mlm_labels.append(
+            torch.tensor(
+                mlm_pred_label_ids
+                + [0] * (max_num_mlm_preds - len(mlm_pred_label_ids)),
+                dtype=torch.long,
+            )
+        )
+        nsp_labels.append(torch.tensor(is_next, dtype=torch.long))
+    return (
+        all_token_ids,
+        all_segments,
+        valid_lens,
+        all_pred_positions,
+        all_mlm_weights,
+        all_mlm_labels,
+        nsp_labels,
+    )
+
+
+# @save
+class _WikiTextDataset(torch.utils.data.Dataset):
+    def __init__(self, paragraphs, max_len):
+        # Input `paragraphs[i]` is a list of sentence strings representing a
+        # paragraph; while output `paragraphs[i]` is a list of sentences
+        # representing a paragraph, where each sentence is a list of tokens
+        paragraphs = [d2l.tokenize(paragraph, token="word") for paragraph in paragraphs]
+        sentences = [sentence for paragraph in paragraphs for sentence in paragraph]
+        self.vocab = d2l.Vocab(
+            sentences, min_freq=5, reserved_tokens=["<pad>", "<mask>", "<cls>", "<sep>"]
+        )
+        # Get data for the next sentence prediction task
+        examples = []
+        for paragraph in paragraphs:
+            examples.extend(
+                _get_nsp_data_from_paragraph(paragraph, paragraphs, self.vocab, max_len)
+            )
+        # Get data for the masked language model task
+        examples = [
+            (_get_mlm_data_from_tokens(tokens, self.vocab) + (segments, is_next))
+            for tokens, segments, is_next in examples
+        ]
+        # Pad inputs
+        (
+            self.all_token_ids,
+            self.all_segments,
+            self.valid_lens,
+            self.all_pred_positions,
+            self.all_mlm_weights,
+            self.all_mlm_labels,
+            self.nsp_labels,
+        ) = _pad_bert_inputs(examples, max_len, self.vocab)
+
+    def __getitem__(self, idx):
+        return (
+            self.all_token_ids[idx],
+            self.all_segments[idx],
+            self.valid_lens[idx],
+            self.all_pred_positions[idx],
+            self.all_mlm_weights[idx],
+            self.all_mlm_labels[idx],
+            self.nsp_labels[idx],
+        )
+
+    def __len__(self):
+        return len(self.all_token_ids)
+
+
+def load_data_wiki(batch_size, max_len):
+    """Load the WikiText-2 dataset."""
+    num_workers = d2l.get_dataloader_workers()
+    data_dir = d2l.download_extract("wikitext-2", "wikitext-2")
+    paragraphs = _read_wiki(data_dir)
+    train_set = _WikiTextDataset(paragraphs, max_len)
+    train_iter = torch.utils.data.DataLoader(
+        train_set, batch_size, shuffle=True, num_workers=num_workers
+    )
+    return train_iter, train_set.vocab
